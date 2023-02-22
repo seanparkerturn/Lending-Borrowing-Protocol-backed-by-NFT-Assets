cbc8b9987b6a57cda667ae263be1382899ac684b
seanparkerturn <124059971+seanparkerturn@users.noreply.github.com>
2023-02-22 19:05:01 +0530

    third

--- APIConsumer.sol
+++ APIConsumer.sol
@@ -1,57 +1,22 @@
-// SPDX-License-Identifier: Unlicensed
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.10;
 
-import "chainlink/contracts/src/v0.8/ChainlinkClient.sol";
+import "@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 
+/**
+ * Request testnet LINK and ETH here: https://faucets.chain.link/
+ * Find information on LINK Token Contracts and get the latest ETH and LINK faucets here: https://docs.chain.link/docs/link-token-contracts/
+ */
 
+/**
+ * THIS IS AN EXAMPLE CONTRACT WHICH USES HARDCODED VALUES FOR CLARITY.
+ * PLEASE DO NOT USE THIS CODE IN PRODUCTION.
+ */
 contract APIConsumer is ChainlinkClient {
-    using Chainlink for Chainlink.Request;
-  
-    uint256 public floorPrice;
-    
-    address private oracle;
-    bytes32 private jobId;
-    uint256 private fee;
 
-    string public Name;
-    string public url;
+    function requestFloorPrice() public returns (bytes32 requestId) {}
 
-
-    constructor(string memory _name, string memory _url) {
-        setPublicChainlinkToken();
-        oracle = 0xc57B33452b4F7BB189bB5AfaE9cc4aBa1f7a4FD8;
-        jobId = "d5270d1c311941d0b08bead21fea7747";
-        fee = 0.1 * 10 ** 18;
-
-        name = _Name;
-        url = _url;
-    }
-    
-   
-    function requestFloorPrice() public returns (bytes32 requestId) 
-    {
-        Chainlink.Request memory request = buildChainlinkRequest(jobId, address(this), this.fulfill.selector);
-        
-        // Set the URL to perform the GET request on
-        request.add("get", url);
-        
-        request.add("path", "collection,stats,floor_price"); // Chainlink nodes 1.0.0 and later support this format
-        
-        // Multiply the result by 1000000000000000000 to remove decimals
-        int timesAmount = 10**18;
-        request.addInt("times", timesAmount);
-        
-        // Sends the request
-        return sendChainlinkRequestTo(oracle, request, fee);
-    }
-    
-    /**
-     * Receive the response in the form of uint256
-     */ 
-    function fulfill(bytes32 _requestId, uint256 _floorPrice) public recordChainlinkFulfillment(_requestId)
-    {
-        floorPrice = _floorPrice;
-    }
+    function fulfill(bytes32 _requestId, uint256 _floorPrice) public recordChainlinkFulfillment(_requestId) {}
 
     // function withdrawLink() external {} - Implement a withdraw function to avoid locking your LINK in the contract
 }
--- USDCVault.sol
+++ USDCVault.sol
@@ -4,90 +4,17 @@ pragma solidity 0.8.10;
 import {Auth} from "solmate/auth/Auth.sol";
 import {ERC4626} from "solmate/mixins/ERC4626.sol";
 
-import {SafeCastLib} from "solmate/utils/SafeCastLib.sol";
-import {SafeTransferLib} from "solmate/utils/SafeTransferLib.sol";
-import {FixedPointMathLib} from "solmate/utils/FixedPointMathLib.sol";
+interface USDCVault is ERC4626, Auth {
 
-import {ERC20} from "solmate/tokens/ERC20.sol";
+  function totalAssets() public view override returns (uint256 totalFunds) {}
 
-contract USDCVault is ERC4626, Auth {
-  using SafeCastLib for uint256;
-  using SafeTransferLib for ERC20;
-  using FixedPointMathLib for uint256;
-  
-  /*///////////////////////////////////////////////////////////////
-                              IMMUTABLES
-  //////////////////////////////////////////////////////////////*/
+  function afterDeposit(uint256 assets, uint256 shares) internal virtual overrides {}
 
-  /// @notice The underlying token the Vault accepts.
-  ERC20 public immutable UNDERLYING;
+  function beforeWithdraw(uint256 assets, uint256 shares) internal virtual overrides {}
 
-  /// @notice The base unit of the underlying token and hence rvToken.
-  /// @dev Equal to 10 ** decimals. Used for fixed point arithmetic.
-  uint256 internal immutable BASE_UNIT;
-  
-  /*///////////////////////////////////////////////////////////////
-                              STORAGE
-  //////////////////////////////////////////////////////////////*/
+  function useFunds(uint256 funds) external requiresAuth {}
 
-  mapping(address => uint256) private getShares; 
-  mapping(address => uint256) private getTotalDeposits;
-
-  uint256 public totalFunds;
-
-  constructor(ERC20 _UNDERLYING)
-      ERC4626(
-          // Underlying token
-          _UNDERLYING,
-          // USDC vault 
-          string(abi.encodePacked(_UNDERLYING.name(), " Vault")),
-          // vUSDC 
-          string(abi.encodePacked("v", _UNDERLYING.symbol()))
-      )
-      Auth(Auth(msg.sender).owner(), Auth(msg.sender).authority())
-  {
-      UNDERLYING = _UNDERLYING;
-
-      BASE_UNIT = 10**decimals;
-
-      // Prevent minting of vTokens until
-      // the initialize function is called.
-      totalSupply = type(uint256).max;
-  }
-
-  function totalAssets() public view override returns (uint256 totalFunds) {
-    return (totalFunds);
-  }
-
-  function afterDeposit(uint256 assets, uint256 shares) internal virtual overrides {
-
-    getShares[msg.sender] += shares;
-
-    getTotalDeposits[msg.sender] += assets;
-
-    totalFunds == assets;
-  }
-
-  function beforeWithdraw(uint256 assets, uint256 shares) internal virtual overrides {
-    getShares[msg.sender] -= shares;
-
-    getTotalDeposits[msg.sender] -= assets;
-
-    totalFunds -= assets;
-  }
-
-  function useFunds(uint256 funds) external requiresAuth {
-
-    UNDERLYING.safeTransferFrom(address(this), msg.sender, funds);
-    totalFunds -= funds;
-    
-  }
-
-  function returnFunds(uint256 funds) external requiresAuth {
-    
-    UNDERLYING.safeTransferFrom(address(this), msg.sender, funds);
-    totalFunds += funds;
-
-  }
+  function returnFunds(uint256 funds) external requiresAuth {}
     
 }
+ 
